b22b456fe07101fd2fa50c8363aa84faf3c67697
Bill Chan <billpwchan@hotmail.com>
2021-07-04 18:39:40 +0100

    update

--- mc_contact_cn_request.js
+++ mc_contact_cn_request.js
@@ -45,7 +45,18 @@ const sendPostRequest = async () => {
             console.log(`Registration Complete: ${fullName} - ${email} - ${password}`)
         })
         .catch(function (error) {
-            console.log(error)
+            if (error.response) {
+                // Request made and server responded
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              } else if (error.request) {
+                // The request was made but no response was received
+                console.log(error.request);
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+              }
         });
 }
 
