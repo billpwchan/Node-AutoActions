77530f737ca79048f65ca1f328aa1f276575b2dc
Bill Chan <billpwchan@hotmail.com>
2021-07-04 18:28:39 +0100

    Update Files

--- mc_contact_cn_request.js
+++ mc_contact_cn_request.js
@@ -2,7 +2,7 @@ const faker = require('faker');
 var randomName = require("chinese-random-name");
 const university = ["University of Oxford", "University of Cambridge", "Imperial College London", "University College London", "London School of Economics and Political Science", "University of Edinburgh", "King’s College London", "University of Manchester", "University of Bristol", "University of Glasgow", "University of Southampton", "University of Birmingham", "Queen Mary, University of London", "University of Warwick", "The University of Sheffield", "University of Liverpool", "The University of Nottingham", "University of Leeds", "University of Exeter"]
 const lead_source = ["LinkedIn领英", "Google/other search", "University seminars", "WeChat", "Sina Weibo", "Facebook", "Other events", "Friends", "Other"]
-const url = Buffer.from("aHR0cDovL3d3dy5tY2l3b3JsZHdpZGUuY29tLmNuL3JlZ19zYXZlMi5waHA/SG9zdD0gd3d3Lm1jaXdvcmxkd2lkZS5jb20uY24mQ29ubmVjdGlvbj0ga2VlcC1hbGl2ZSZPcmlnaW49IGh0dHA6Ly93d3cubWNpd29ybGR3aWRlLmNvbS5jbiZSZWZlcmVyPSBodHRwOi8vd3d3Lm1jaXdvcmxkd2lkZS5jb20uY24vcmVnLnBocA==", 'base64').toString('binary')
+const url = Buffer.from("aHR0cDovL3d3dy5tY2l3b3JsZHdpZGUuY29tLmNuL3JlZ19zYXZlMjIwMjEwNTIyNS5waHA=", 'base64').toString('binary')
 var axios = require('axios');
 var FormData = require('form-data');
 const txtgen = require('txtgen');
@@ -45,6 +45,7 @@ const sendPostRequest = async () => {
             console.log(`Registration Complete: ${fullName} - ${email} - ${password}`)
         })
         .catch(function (error) {
+            console.log(error)
         });
 }
 
